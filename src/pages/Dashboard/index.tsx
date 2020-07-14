c72edec35ffb6f7dca6410557f17ae6bd300353b
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-07-14 13:00:05 -0300

    Conexão com api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,86 +1,69 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    console.log(response.data);
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no github</Title>
 
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/20937753?s=460&u=1920f44394994ec9e0603aa36987bfa99242a242&v=4"
-            alt="João Vítor"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>joaovitormarquim/adonis_tutorial</strong>
-            <p>
-              Help people interested in this repository understand your project
-              by adding a README.
-            </p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/20937753?s=460&u=1920f44394994ec9e0603aa36987bfa99242a242&v=4"
-            alt="João Vítor"
-          />
-
-          <div>
-            <strong>joaovitormarquim/adonis_tutorial</strong>
-            <p>
-              Help people interested in this repository understand your project
-              by adding a README.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/20937753?s=460&u=1920f44394994ec9e0603aa36987bfa99242a242&v=4"
-            alt="João Vítor"
-          />
-
-          <div>
-            <strong>joaovitormarquim/adonis_tutorial</strong>
-            <p>
-              Help people interested in this repository understand your project
-              by adding a README.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="/">
-          <img
-            src="https://avatars3.githubusercontent.com/u/20937753?s=460&u=1920f44394994ec9e0603aa36987bfa99242a242&v=4"
-            alt="João Vítor"
-          />
-
-          <div>
-            <strong>joaovitormarquim/adonis_tutorial</strong>
-            <p>
-              Help people interested in this repository understand your project
-              by adding a README.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
